4f7c6cfd1378dcaa3da51923147add6dcf55cfb0
David Andrews <andr0182@algonquinlive.com>
2012-08-17 18:54:48 -0400

    Updated .htaccess and db.php for deployment on phpfog.

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,15 @@
 <?php
 
+$user = getenv('MYSQL_USERNAME');
+$pass = getenv('MYSQL_PASSWORD');
+$host = getenv('MYSQL_HOST');
+$dbname = getenv('MYSQL_DBNAME');
+
 $photofinder = getenv('DB_USER');
 $pass = getenv('DB_PASS');
-$data_source = getenv('DATA_SOURCE');
+//$data_source = getenv('DATA_SOURCE');
+
+$data_source = sprintf('mysql:host=%s;dbname=%s',$host, $dbname);
 
 $db = new PDO($data_source, $photofinder, $pass);
 
